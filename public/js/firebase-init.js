5f860d366e1d5078d14737201f2294d5e0c74e13
vnvcpas-tools <vnvcpas.excelimporter@gmail.com>
2026-04-07 20:57:01 +0800

    Create firebase-init.js

---
+++ public/js/firebase-init.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-storage.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_reVQpx1F0QnynRCPFj0T1btKAoZLkK4",
+    authDomain: "excel-transaction-importer.firebaseapp.com",
+    projectId: "excel-transaction-importer",
+    storageBucket: "excel-transaction-importer.firebasestorage.app",
+    messagingSenderId: "1080856597867",
+    appId: "1:1080856597867:web:767356c09bb243d44ca82e",
+    measurementId: "G-6PZ1KPJDGX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
